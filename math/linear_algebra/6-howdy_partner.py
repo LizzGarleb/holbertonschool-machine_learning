57a8310e4815d5bc6e5be59f406ac02ef31b492e
LizzGarleb <redscorpion3108@gmail.com>
2023-07-25 21:47:53 -0400

    Task 6: Howdy Partner

---
+++ math/linear_algebra/6-howdy_partner.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""
+    Module content:
+        - cat_arrays - Concatenates two arrays
+"""
+
+
+def cat_arrays(arr1, arr2):
+    """
+        cat_arrays - Concatenates two arrays
+
+        @arr1 & arr2: Arrays that will be concatenated
+
+        Return: A new list is returned upon success
+    """
+    return arr1 + arr2
