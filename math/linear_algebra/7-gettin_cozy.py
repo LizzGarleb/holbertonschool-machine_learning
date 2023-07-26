4a03964fac8bd6f381b6d89f5826b5efdb180c9f
LizzGarleb <redscorpion3108@gmail.com>
2023-07-26 15:27:47 -0400

    Task 7: Getting cozy

---
+++ math/linear_algebra/7-gettin_cozy.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+"""
+    Module content:
+        - cat_matrices2D - Concatenates two matrices along
+                            a specific axis
+"""
+
+
+def cat_matrices2D(mat1, mat2, axis=0):
+    """
+        cat_matrices2D - Concatenates two matrices along a
+                        specific axis
+
+        @mat1 & @mat2: Matrix to concatenate
+        @axis: Options to concatenate the matrix
+
+        Return: a new matrix concatenated
+
+        Description: When axis is 0, mat2 will be added at
+        the end of mat1. If axis is 1, concatenation will be
+        per column
+    """
+
+    newMatrix = []
+
+    if axis == 0:
+        newMatrix = mat1 + mat2
+
+    if axis == 1:
+        for i in range(len(mat1)):
+            newMatrix.append(mat1[i] + mat2[i])
+
+    return newMatrix
