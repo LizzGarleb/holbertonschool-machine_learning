245be617ff54d201887795a65bbdf9cf0b291d5a
LizzGarleb <redscorpion3108@gmail.com>
2023-08-07 11:39:36 -0400

    documentation updated & pycodestyle passed

--- math/probability/poisson.py
+++ math/probability/poisson.py
@@ -1,11 +1,31 @@
 #!/usr/bin/env python3
+"""
+    Module Content:
+        - A Poisson class that represent a poisson distribution.
+"""
+
 
 class Poisson:
+
+    """
+        Functions:
+            - __init__(self, data=None, lambtha=1.): Class constructor
+            - pmd: Calculate the value of the PMF for a given number of
+                    "successes"
+                    Returns: the PMF value of k
+    """
+
     def __init__(self, data=None, lambtha=1.):
         """
-            ___init__: Class constructor
+            ___init__: class constructor
+
             @data: list of data to be used to estimate the distribution
             @lambtha: expected number of occurences in a given time frame
+
+            Raises:
+                ValueError: lambtha must be a positive value
+                TypeError: data must be a list
+                ValueError: data must contain multiple values
         """
 
         if data is not None:
@@ -18,4 +38,3 @@ class Poisson:
             if lambtha <= 0:
                 raise ValueError("lambtha must be a positive value")
             self.lambtha = float(lambtha)
- 
