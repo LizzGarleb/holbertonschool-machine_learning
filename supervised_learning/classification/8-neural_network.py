898011a0077860f0e847e306b40f9a101edcd7ca
LizzGarleb <redscorpion3108@gmail.com>
2023-08-23 12:57:42 -0400

    Correcting task 8 and done task 9

--- supervised_learning/classification/8-neural_network.py
+++ supervised_learning/classification/8-neural_network.py
@@ -32,10 +32,10 @@ class NeuralNetwork:
         if nodes < 1:
             raise ValueError("nodes must be a positive integer")
         
-        self.W1 = np.random.normal(size=(1, nx))
-        self.b1 = 0
+        self.W1 = np.random.normal(size=(nodes, nx))
+        self.b1 = np.zeros((nodes, 1))
         self.A1 = 0
 
-        self.W2 = np.random.normal(size=(1, nx))
+        self.W2 = np.random.normal(size=(1, nodes))
         self.b2 = 0
         self.A2 = 0
--- supervised_learning/classification/9-neural_network.py
+++ supervised_learning/classification/9-neural_network.py
@@ -32,11 +32,11 @@ class NeuralNetwork:
         if nodes < 1:
             raise ValueError("nodes must be a positive integer")
         
-        self.__W1 = np.random.normal(size=(1, nx))
-        self.__b1 = 0
+        self.__W1 = np.random.normal(size=(nodes, nx))
+        self.__b1 = np.zeros((nodes, 1))
         self.__A1 = 0
 
-        self.__W2 = np.random.normal(size=(1, nx))
+        self.__W2 = np.random.normal(size=(1, nodes))
         self.__b2 = 0
         self.__A2 = 0
 
