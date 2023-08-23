18ab99151366c0029abda84adb4cbc6c928a7335
LizzGarleb <redscorpion3108@gmail.com>
2023-08-23 12:40:49 -0400

    task 8

---
+++ supervised_learning/classification/8-neural_network.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python3
+"""
+    Module Content:
+        - A Neuron class that defines a single neuron
+        performing binary classification
+"""
+import numpy as np
+
+
+class NeuralNetwork:
+
+    def __init__(self, nx, nodes):
+        """
+            __init__: class constructor
+
+            Input:
+                @nx: the number of input features
+                @nodes: the number of nodes found in the hidden layer
+
+            Raises:
+                TypeError:
+                ValueError:
+                TypeError:
+                ValueError:
+        """
+        if type(nx) is not int:
+            raise TypeError("nx must be an integer")
+        if nx < 1: 
+            raise ValueError("nx must be a positive integer")
+        if type(nodes) is not int:
+            raise TypeError("node must be a integer")
+        if nodes < 1:
+            raise ValueError("nodes must be a positive integer")
+        
+        self.W1 = np.random.normal(size=(1, nx))
+        self.b1 = 0
+        self.A1 = 0
+
+        self.W2 = np.random.normal(size=(1, nx))
+        self.b2 = 0
+        self.A2 = 0
