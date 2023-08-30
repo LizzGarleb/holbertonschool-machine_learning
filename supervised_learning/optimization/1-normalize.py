bf0df9ef3448d5095b3694440535ebbc4c43dfca
LizzGarleb <redscorpion3108@gmail.com>
2023-08-30 12:19:59 -0400

    pycodestyle  pass

--- supervised_learning/optimization/1-normalize.py
+++ supervised_learning/optimization/1-normalize.py
@@ -5,7 +5,7 @@ import numpy as np
 
 
 def normalize(X, m, s):
-    """ normalizes (standardizes) a matrix 
+    """ normalizes (standardizes) a matrix
         Return: The normalized X matrix
     """
     return ((X - m) / s)
