a67885327aa583d63b19c184aecdca16d24441da
LizzGarleb <redscorpion3108@gmail.com>
2023-09-01 13:40:32 -0400

    task 12

--- supervised_learning/optimization/12-learning_rate_decay.py
+++ supervised_learning/optimization/12-learning_rate_decay.py
@@ -11,4 +11,4 @@ def learning_rate_decay(alpha, decay_rate, global_step, decay_step):
     decay
     """
     return tf.train.inverse_time_decay(alpha, global_step, decay_step,
-                                        decay_rate, staircase=True)
+                                       decay_rate, staircase=True)
