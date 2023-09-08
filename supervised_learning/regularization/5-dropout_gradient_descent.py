77860b20727e9a185d5f13a56cfe73fe7513f4d0
LizzGarleb <redscorpion3108@gmail.com>
2023-09-08 16:33:26 -0400

    task 5

--- supervised_learning/regularization/5-dropout_gradient_descent.py
+++ supervised_learning/regularization/5-dropout_gradient_descent.py
@@ -1,29 +1,21 @@
 #!/usr/bin/env python3
-"""Dropout Forward Propagation"""
+""" regularizei """
 import numpy as np
 
 
 def dropout_gradient_descent(Y, weights, cache, alpha, keep_prob, L):
-    """Conducts forward propagation using Dropout"""
-    n_layers = range(L, 0, -1)
+    """ gradient descent with dropout"""
     m = Y.shape[1]
-    dZ_prev = 0
-    w = weights.copy()
-
-    for i in n_layers:
-        A = cache['A{}'.format(i)]
-        A_prev = cache['A{}'.format(i - 1)]
-        weights_i = w.get('W{}'.format(i))
-        weights_n = w.get('W{}'.format(i + 1))
-        biases = w.get('b' + str(i))
-        if i == L:
-            dZ = A - Y
-        else:
-            dZ = np.matmul(weights_n.T, dZ_prev) * (1 - (A * A))
-            dZ *= cache['D{}'.format(i)]
-            dZ /= keep_prob
-        dW = np.matmul(dZ, A_prev.T) / m
-        db = np.sum(dZ, axis=1, keepdims=True) / m
-        weights['W' + str(i)] = weights_i - (dW * alpha)
-        weights['b' + str(i)] = biases - (db * alpha)
-        dZ_prev = dZ
+    dz = cache["A" + str(L)] - Y
+    for x in range(L, 0, -1):
+        A_prev = cache["A" + str(x - 1)]
+        dw = np.matmul(dz, A_prev.T) / m
+        db = np.sum(dz, axis=1, keepdims=True) / m
+        inv = 1 - np.square(A_prev)
+        da = np.matmul(weights["W" + str(x)].T, dz)
+        if x > 1:
+            da = da * cache["D" + str(x - 1)]
+            da = da / keep_prob
+        dz = da * inv
+        weights["W" + str(x)] -= alpha * dw
+        weights["b" + str(x)] -= alpha * db
