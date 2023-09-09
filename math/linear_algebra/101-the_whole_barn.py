de8fcff81c64e1244d68702c880954a859597b49
LizzGarleb <redscorpion3108@gmail.com>
2023-09-09 00:09:46 -0400

    doing it the hard way, no imports

--- math/linear_algebra/101-the_whole_barn.py
+++ math/linear_algebra/101-the_whole_barn.py
@@ -4,15 +4,17 @@ Add two matrices
 """
 
 
-import numpy as np
-
 def add_matrices(mat1, mat2):
-    # Convert mat1 and mat2 to NumPy arrays
-    mat1 = np.array(mat1)
-    mat2 = np.array(mat2)
-
     # Check if mat1 and mat2 have the same shape
-    if mat1.shape != mat2.shape:
+    if len(mat1) != len(mat2) or len(mat1[0]) != len(mat2[0]):
         return None  # Return None if matrices have different shapes
 
-    return mat1 + mat2
+    # Initialize an empty result matrix with the same shape
+    result_matrix = [[0 for _ in range(len(mat1[0]))] for _ in range(len(mat1))]
+
+    # Add the matrices element-wise
+    for i in range(len(mat1)):
+        for j in range(len(mat1[0])):
+            result_matrix[i][j] = mat1[i][j] + mat2[i][j]
+
+    return result_matrix
