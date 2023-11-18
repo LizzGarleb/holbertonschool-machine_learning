04147ed827c4a4e93212278ab43cb5eabe8c49c0
LizzGarleb <redscorpion3108@gmail.com>
2023-11-17 22:53:34 -0400

    task 10

---
+++ unsupervised_learning/clustering/test/10-main.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+import numpy as np
+import matplotlib.pyplot as plt
+kmeans = __import__('10-kmeans').kmeans
+
+if __name__ == "__main__":
+    np.random.seed(0)
+    a = np.random.multivariate_normal([30, 40], [[16, 0], [0, 16]], size=50)
+    b = np.random.multivariate_normal([10, 25], [[16, 0], [0, 16]], size=50)
+    c = np.random.multivariate_normal([40, 20], [[16, 0], [0, 16]], size=50)
+    d = np.random.multivariate_normal([60, 30], [[16, 0], [0, 16]], size=50)
+    e = np.random.multivariate_normal([20, 70], [[16, 0], [0, 16]], size=50)
+    X = np.concatenate((a, b, c, d, e), axis=0)
+    np.random.shuffle(X)
+
+    C, clss = kmeans(X, 5)
+    print(C)
+    plt.scatter(X[:, 0], X[:, 1], s=10, c=clss)
+    plt.scatter(C[:, 0], C[:, 1], s=50, marker='*', c=list(range(5)))
+    plt.show()
+
+# Expected Output:
+# [[ 9.92511389 25.73098987]
+#  [30.06722465 40.41123947]
+#  [39.62770705 19.89843487]
+#  [59.22766628 29.19796006]
+#  [20.0835633  69.81592298]]
