bd7c29a1fbd099fcf689f5f82a0b10ebfac9bccd
LizzGarleb <redscorpion3108@gmail.com>
2023-11-17 21:40:59 -0400

    task 4

---
+++ unsupervised_learning/clustering/test/4-main.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+
+import numpy as np
+initialize = __import__('4-initialize').initialize
+
+if __name__ == '__main__':
+    np.random.seed(11)
+    a = np.random.multivariate_normal([30, 40], [[75, 5], [5, 75]], size=10000)
+    b = np.random.multivariate_normal([5, 25], [[16, 10], [10, 16]], size=750)
+    c = np.random.multivariate_normal([60, 30], [[16, 0], [0, 16]], size=750)
+    d = np.random.multivariate_normal([20, 70], [[35, 10], [10, 35]], size=1000)
+    X = np.concatenate((a, b, c, d), axis=0)
+    np.random.shuffle(X)
+    pi, m, S = initialize(X, 4)
+    print(pi)
+    print(m)
+    print(S)
+
+# Expected Output:
+# [0.25 0.25 0.25 0.25]
+# [[54.73711515 31.81393242]
+#  [16.84012557 31.20248225]
+#  [21.43215816 65.50449077]
+#  [32.3301925  41.80664127]]
+# [[[1. 0.]
+#   [0. 1.]]
+
+#  [[1. 0.]
+#   [0. 1.]]
+
+#  [[1. 0.]
+#   [0. 1.]]
+
+#  [[1. 0.]
+#   [0. 1.]]]
