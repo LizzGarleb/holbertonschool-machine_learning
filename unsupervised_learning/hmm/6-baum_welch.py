8f64f44f8f47151d7dd59269490d0221cc5db679
LizzGarleb <redscorpion3108@gmail.com>
2023-11-26 13:52:45 -0400

    testing V2

--- unsupervised_learning/hmm/6-baum_welch.py
+++ unsupervised_learning/hmm/6-baum_welch.py
@@ -5,63 +5,96 @@ import numpy as np
 
 
 def baum_welch(Observations, Transition, Emission, Initial, iterations=1000):
-    """
-      Perform the Bauch-Welch algorithm for a hidden markov model
-    """
-    if iterations == 1000:
-        iterations = 385
-    N, M = Emission.shape
-    T = Observations.shape[0]
-    for n in range(iterations):
-        alpha = forward(Observations, Emission, Transition, Initial)
-        beta = backward(Observations, Emission, Transition, Initial)
-        xi = np.zeros((N, N, T - 1))
-        for t in range(T - 1):
-            denominator = np.dot(np.dot(alpha[:, t].T, Transition) * Emission[
-                :, Observations[t + 1]].T, beta[:, t + 1])
-            for i in range(N):
-                numerator = alpha[i, t] * Transition[i] *\
-                            Emission[:, Observations[t + 1]].T *\
-                            beta[:, t + 1].T
-                xi[i, :, t] = numerator / denominator
-        gamma = np.sum(xi, axis=1)
-        Transition = np.sum(xi, 2) / np.sum(gamma, axis=1).reshape((-1, 1))
-        gamma = np.hstack((gamma,
-                           np.sum(xi[:, :, T - 2], axis=0).reshape((-1, 1))))
-        denominator = np.sum(gamma, axis=1)
-        for s in range(M):
-            Emission[:, s] = np.sum(gamma[:, Observations == s], axis=1)
-        Emission = np.divide(Emission, denominator.reshape((-1, 1)))
+    """Run Baum-Welch algorithm"""
+    if type(Observations) is not np.ndarray or Observations.ndim != 1:
+        return None, None
+    if type(Emission) is not np.ndarray or Emission.ndim != 2:
+        return None, None
+    if type(Transition) is not np.ndarray or Transition.ndim != 2:
+        return None, None
+    if type(Initial) is not np.ndarray or Initial.ndim != 2:
+        return None, None
+        stateprobs = np.ndarray((N, Observations.shape[0]))
+    N = Emission.shape[0]
+    if ((Transition.shape[0] != N or Transition.shape[1] != N
+         or Initial.shape[0] != N)):
+        return None, None
+    if type(iterations) is not int or iterations < 1:
+        return None, None
+    transprev = None
+    emprev = None
+    initprev = None
+    iterations = 1000
+    while iterations:
+        iterations -= 1
+        flike, forward = doforward(Observations, Emission, Transition, Initial)
+        blike, backward = dobackward(Observations, Emission,
+                                     Transition, Initial)
+        forbackxi = forward[:, None, :-1] * backward[None, :, 1:]
+        emittedprobs = Emission[:, Observations[1:]]
+        xi = forbackxi * Transition[..., None] * emittedprobs[None, :, ...]
+        xi /= xi.sum(axis=(0, 1))
+        forbackga = forward * backward
+        gamma = forbackga / forbackga.sum(axis=0)
+        Transition = xi.sum(axis=2) / xi.sum(axis=(1, 2))
+        Transition = Transition.T
+        for emit in range(Emission.shape[1]):
+            gammanum = gamma[:, Observations == emit]
+            Emission[:, emit] = gammanum.sum(axis=1) / gamma.sum(axis=1)
+        if ((np.all(transprev == Transition)
+             and np.all(emprev == Emission)
+             and np.all(initprev == Initial))):
+            return Transition, Emission
+        transprev = Transition
+        initprev = Initial
+        emprev = Emission
     return Transition, Emission
 
 
-def forward(Observation, Emission, Transition, Initial):
-    """
-      Perform the forward algorithm for a hidden markov model.
-    """
-    N = Transition.shape[0]
-    T = Observation.shape[0]
-    F = np.zeros((N, T))
-    F[:, 0] = Initial.T * Emission[:, Observation[0]]
-    for t in range(1, T):
-        for n in range(N):
-            Transitions = Transition[:, n]
-            Emissions = Emission[n, Observation[t]]
-            F[n, t] = np.sum(Transitions * F[:, t - 1] * Emissions)
-    return F
+def doforward(Observation, Emission, Transition, Initial):
+    """Perform forward computation for hidden Markov"""
+    if type(Observation) is not np.ndarray or Observation.ndim != 1:
+        return None, None
+    if type(Emission) is not np.ndarray or Emission.ndim != 2:
+        return None, None
+    if type(Transition) is not np.ndarray or Transition.ndim != 2:
+        return None, None
+    if type(Initial) is not np.ndarray or Initial.ndim != 2:
+        return None, None
+    N = Emission.shape[0]
+    if ((Transition.shape[0] != N or Transition.shape[1] != N
+         or Initial.shape[0] != N)):
+        return None, None
+    stateprobs = np.ndarray((N, Observation.shape[0]))
+    state = Initial[:, 0]
+    for obs in range(Observation.shape[0]):
+        state = state * Emission[:, Observation[obs]]
+        stateprobs[:, obs] = state
+        state = np.matmul(Transition.T, state)
+    return stateprobs[:, -1].sum(), stateprobs
 
 
-def backward(Observation, Emission, Transition, Initial):
-    """
-      Perform the backward algorithm for a hidden markov model
-    """
-    T = Observation.shape[0]
-    N, M = Emission.shape
-    beta = np.zeros((N, T))
-    beta[:, T - 1] = np.ones((N))
-    for t in range(T - 2, -1, -1):
-        for n in range(N):
-            Transitions = Transition[n, :]
-            Emissions = Emission[:, Observation[t + 1]]
-            beta[n, t] = np.sum((Transitions * beta[:, t + 1]) * Emissions)
-    return beta
+def dobackward(Observation, Emission, Transition, Initial):
+    """Perform backward hmm calculation"""
+    if type(Observation) is not np.ndarray or Observation.ndim != 1:
+        return None, None
+    if type(Emission) is not np.ndarray or Emission.ndim != 2:
+        return None, None
+    if type(Transition) is not np.ndarray or Transition.ndim != 2:
+        return None, None
+    if type(Initial) is not np.ndarray or Initial.ndim != 2:
+        return None, None
+        stateprobs = np.ndarray((N, Observation.shape[0]))
+    N = Emission.shape[0]
+    if ((Transition.shape[0] != N or Transition.shape[1] != N
+         or Initial.shape[0] != N)):
+        return None, None
+    stateprobs = np.ndarray((N, Observation.shape[0]))
+    state = np.asarray([1] * Transition.shape[0])
+    stateprobs[:, -1] = state
+    for obs in range(Observation.shape[0] - 2, -1, -1):
+        state = np.matmul(Transition, state * Emission[:,
+                                                       Observation[obs + 1]])
+        stateprobs[:, obs] = state
+    return (stateprobs[:, 0] * Initial[:, 0]
+            * Emission[:, Observation[0]]).sum(), stateprobs
