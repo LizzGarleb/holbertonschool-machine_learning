f03e41f86a0ab14d4bc009d9f7ba7e7d26b4f1a2
LizzGarleb <redscorpion3108@gmail.com>
2023-11-26 13:39:23 -0400

    task 2

---
+++ unsupervised_learning/hmm/2-absorbing.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+"""Module Absorbing"""
+
+import numpy as np
+
+
+def absorbing(P):
+    """
+      Determine if a markov chain is absorbing.
+    """
+    if len(P.shape) != 2:
+        return None
+    n1, n2 = P.shape
+    if (n1 != n2) or type(P) is not np.ndarray:
+        return None
+    D = np.diagonal(P)
+    if (D == 1).all():
+        return True
+    if not (D == 1).any():
+        return False
+    for i in range(n1):
+        for j in range(n2):
+            if (i == j) and (i + 1 < len(P)):
+                if P[i + 1][j] == 0 and P[i][j + 1] == 0:
+                    return False
+    return True
