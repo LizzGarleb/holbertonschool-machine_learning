23c4aadbaec16b3a9fc6a87ef70b900a8aac2d74
LizzGarleb <redscorpion3108@gmail.com>
2024-03-25 10:47:03 -0400

    task 30

---
+++ pipeline/databases/30-all.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""
+  Module list_all
+"""
+
+
+def list_all(mongo_collection):
+    """
+    List all documents in a collection.
+    """
+    all_docs = []
+    collection = mongo_collection.find()
+    for document in collection:
+        all_docs.append(document)
+    return all_docs
